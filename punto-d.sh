eb10b56174819aee0238232aac455baac44ebf62
vazquezsantiago <vazquezsantiago47606@gmail.com>
2025-10-20 22:43:13 +0000

    punto D

---
+++ punto-d.sh
@@ -0,0 +1,2 @@
+mkdir -p ~/Ejercicio_D/{rx,tx}/{lotes_{1..20}}
+mkdir -p ~/Ejercicio_D/rx/extras_{1..100}
---
+++ punto_b.sh
@@ -0,0 +1,4 @@
+groupadd p1c1_2024_g1
+sudo useradd -g p1c1_2024_g1 p1c1_2024_g1
+sudo passwd p1c1_2024_g1
+clave 1
